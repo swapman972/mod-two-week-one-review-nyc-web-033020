e075db1bef4950426eba54f7a8e1bba7db353828
swapman972 <jordan.melidor@hotmail.fr>
2020-04-23 17:37:01 -0400

    we are trying

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -2,6 +2,39 @@ class SeasController < ApplicationController
   #define your controller actions here
 
 
+  def index
+    @seas = Sea.all
+  end
+  
+  def show
+    @sea = Sea.find(params[:id])
+  end
+
+  def new
+    @sea = Sea.new
+  end
+
+  def create
+    @sea = Sea.create(sea_params)
+    redirect_to "/seas/#{@sea.id}"
+  end
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end
+
+  def update
+    sea = Sea.find(params[:id])
+    sea.update(sea_params)
+    redirect to "/seas/#{@sea.id}"
+  end
+
+  def destroy
+    @sea = Sea.destroy(params[:id]) 
+    redirect_to "/seas"
+  end
+  
+
   private
   # In controller actions, use this private method to access sea params from forms.
   # Example: @sea.update(sea_params)
@@ -9,5 +42,4 @@ class SeasController < ApplicationController
   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
-
 end
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  #if the user writes /seas, the following goes to seas_controller
+  #In seas_controller it looks for def seas
+  get 'seas', to: 'seas#index'
+  get 'seas/new', to: 'seas#new'
+  get 'seas/:id', to: 'seas#show', as: 'sea'
+  get 'seas/:id/edit', to: 'seas#edit'
+  post 'seas', to: 'seas#create'
+
+  put '/seas/:id', to: 'seas#update'
+  delete '/seas/:id', to: 'seas#destroy'
+
 end
